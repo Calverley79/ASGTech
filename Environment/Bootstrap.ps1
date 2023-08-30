c3212b7189599e5b77114443b759b493470077d8
Chris Calverley <calverley79@yahoo.com>
2023-08-30 14:30:55 -0500

    commit no change

---
+++ Environment/Bootstrap.ps1
@@ -0,0 +1,34 @@
+
+
+
+$bootstraploaded = $true
+
+function Write-log {
+    param(
+        [Parameter(Mandatory=$false)][string]$Message,
+        [Parameter(Mandatory=$False)][ValidateSet('Log','ERROR','Data')][String]$Type = 'Log'
+      )
+      Set-Location C:\Temp
+      $MyLogName = "$($MyInvocation.ScriptName)"
+      $LogName = (($MyLogName).Split('\')[$(($MyLogName).Split('\')).Count - 1]).Replace('.ps1','')
+      $scriptLog = "$LogName.log"
+    if (!(Test-Path 'C:\Temp')) {
+        New-Item -ItemType Directory -Name .\Temp
+    }
+    if (!(Test-Path "C:\Temp\$scriptLog")) {
+        New-Item -ItemType File -Name $scriptLog
+        $MyDate = Get-Date -Format s
+        Add-Content -Path "$scriptLog" -Value "----------------------------------------------"
+        Add-Content -Path "$scriptLog" -Value "$MyDate - $Type - $MyLogName "
+        Add-Content -Path "$scriptLog" -Value "$MyDate - $Type - $Message"
+    } else {
+        $MyDate = Get-Date -Format s
+        $Lastrun = (Get-Content $scriptLog -Tail 1).Split(' ')
+        $lastruncomparor = ([datetime]$lastrun[0]).AddMinutes(30)
+        If ($MyDate -lt $lastruncomparor) {
+            Add-Content -Path  "$scriptLog" -Value "----------------------------------------------""----------------------------------------------"
+            Add-Content -Path "$scriptLog" -Value "$MyDate - $Type - $MyLogName"
+        }
+        Add-Content -Path "$scriptLog" -Value "$MyDate - $Type - $Message"
+    }
+}
